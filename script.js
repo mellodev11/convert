8600b179be96cd9e75c9793d9988f2f9546e6701
mellodev11 <robertale45@gmail.com>
2024-08-09 19:50:36 -0300

    finalizando função convertCurrency

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+
 const frm = document.querySelector('form')
 const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
+const footer = document.querySelector('main footer')
+const description = document.getElementById('description')
+
+
 
 //MANIPULANDO O INPUT AMOUNT PARA RECEBER SOMENTE NUMEROS
 amount.addEventListener('input', () =>{
@@ -14,7 +24,29 @@ amount.addEventListener('input', () =>{
     frm.onsubmit = (e) =>{
         e.preventDefault()
 
-        console.log(currency.value)
+        switch(currency.value){
+            case "USD":
+                convertCurrency(amount.value, USD, "US$")
+                break
+                case "EUR":
+                    convertCurrency(amount.value, EUR, "€")
+                    break
+                    case "GBP": 
+                    convertCurrency(amount.value, GBP, "£")
+                    break
+        }
     }
+    //função converter a moeda
+    function convertCurrency(amount, price, symbol){
+        try {
+          description.textContent = `${symbol} 1 = ${price}`
+            footer.classList.add("show-result")
+        } catch (error) {
+            footer.classList.remove("show-result")
+            alert("não foi possivel converter")
+        }
+    }
+
+
 
     
